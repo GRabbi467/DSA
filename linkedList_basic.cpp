593444e82dcf07da61ed07d01696c50a61d33435
Golam Rabbi <57221660+GRabbi467@users.noreply.github.com>
2023-11-11 21:56:00 +0600

    Add files via upload

--- linkedList_basic.cpp
+++ linkedList_basic.cpp
@@ -19,6 +19,12 @@ Node* create_node(int item,Node* next){
 
     return new_node;
 }
+
+void insert_node(int item ,Node* node){
+    Node* new_node = create_node(item,node->next);
+    node->next = new_node;
+
+}
 Node* prepend(int item, Node* head){
     Node* new_node = create_node(item,head);
     return new_node;
@@ -36,12 +42,27 @@ Node* append(int item, Node* head){
     return head;
 }
 
-void printLL(Node* head){
+
+Node* remove_node(Node* node, Node* head){
+    //check if the node is the first node of the LL
+    if(node == head){
+        head = node->next;
+        delete node;
+
+        return head;
+    }
     Node* current_node = head;
-    while(current_node != nullptr){
-        cout<<current_node->data<<endl;
+
+    while(current_node->next != nullptr){
+        if(current_node->next == node){
+            break;
+        }
         current_node = current_node->next;
     }
+    if(current_node == nullptr) return head;
+    current_node->next = node->next;
+    delete node;
+    return head;
 }
 Node* ll_reverse(Node* head){
     Node* prev = nullptr;
@@ -56,16 +77,26 @@ Node* ll_reverse(Node* head){
     }
     return prev;
 }
+void printLL(Node* head){
+    Node* current_node = head;
+    while(current_node != nullptr){
+        cout<<current_node->data<<endl;
+        current_node = current_node->next;
+    }
+}
+
 int main(){
-    Node *n1,*n2,*head;
+    Node *n1,*n2,*n3,*head;
     n1 = create_node(20,nullptr);
     n2 = create_node(15,n1);
     head = n2;
     head = prepend(10,head);
     //printLL(head);
     head = append(25,head);
-   // printLL(head);
-    head = ll_reverse(head);
+    insert_node(30,n2);
+    //head = remove_node(n2,head);
+
+    //head = ll_reverse(head);
     printLL(head);
 
     return 0;
